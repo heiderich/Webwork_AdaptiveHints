85bdca8fb3aaf782031b0b5b7f9a2b98b6febfc8
Zhen Zhai <zzhai@eng.ucsd.edu>
2015-10-08 12:45:01 -0700

    new filter functions for testing

---
+++ src/servers/rest_server/filters/C_Week2_13_1_test.py
@@ -0,0 +1,7 @@
+def C_Week2_13_1_test(params):
+	#{'string': '', 'correct_eval': None, 'correct_tree': []], 'evaled': None, 'correct_string': '', 'parsed': []}
+	answer = params["string"]
+	if answer == "good":
+		return "not good right?[_____]{'right'}"
+	else:
+		return ""
