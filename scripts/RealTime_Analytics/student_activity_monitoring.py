430d456b447792361d9f187d5a60476249685ef4
Zhen Zhai <zzhai@eng.ucsd.edu>
2015-10-07 17:14:59 -0700

    Merge branch 'master' into Zhen

---
+++ scripts/RealTime_Analytics/student_activity_monitoring.py
@@ -0,0 +1,122 @@
+import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import paramiko
+
+#these parameters need to be set for each TA
+IP = "webwork.cse.ucsd.edu"
+USERNAME = "yfreund"
+set_id='Week2'
+ssh_private_key='/Users/yoavfreund/.ssh/id_dsa'
+
+def get_table(set_id):
+    """ Connect to database, get _answer_by_part table, parse it, 
+    and return it as pandas dataframe"""
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(IP, username='yfreund', key_filename=ssh_private_key)
+
+    command="""mysql -u readonly -preadonly webwork -e 'select *,UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(A.timestamp) as gap
+from CSE103_Fall2015_answers_by_part as A 
+where A.set_id = "%s" 
+    and YEAR(A.timestamp)=2015  
+    order by A.timestamp;'
+"""%set_id
+
+    # and UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(A.timestamp) < 3600
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
+    #ssh_stderr.read()
+    data = ssh_stdout.readlines()
+    print 'number of lines in response=',len(data)
+    line = data[0].strip()
+    header = line.split('\t')
+
+    table = []
+    for line in data[1:]:
+        line=line.strip();
+        _id,user_id,answer_id,answer_string,score,problem_id,\
+            set_id,part_id,timestamp,gap = line.split('\t')
+        problem_id=int(problem_id)
+        part_id=int(part_id)
+        score=int(score)
+        gap=int(gap)+7*3600 # I am consistently getting time stamps that 
+                            # are 7 hours in the future, corresponding to GMT, resulting in negative gaps
+        
+        table.append((_id,user_id,answer_id,answer_string,score,problem_id,set_id,part_id,timestamp,gap))  
+    
+    df = pd.DataFrame(table,columns=header)
+    ssh.close()
+    return df    
+
+def total_time_trying(times,maxgap=5):
+    """ Compute the total time in a given sequence, 
+    where gaps large than maxgap minutes are set to maxgap """
+    total=datetime.timedelta()
+    max_time=datetime.timedelta(minutes=maxgap)
+    t0=datetime.datetime.strptime(times[0],'%Y-%m-%d %H:%M:%S')
+    for i in range(1,len(times)):
+        t1=datetime.datetime.strptime(times[i],'%Y-%m-%d %H:%M:%S')
+        gap=t1-t0
+        if gap>max_time:
+            #print 'clipped',str(gap)
+            gap=max_time
+        #else:
+        #    print str(gap),
+        total+=gap
+        t0=t1
+    return total,len(times)
+
+
+ranges=(('10 min',600),('1 hour',3600),('10 hours',36000),('anything',1000000))
+def calc_gaps(gap_trace):
+    """ Given a list of times, return a list of counts according to ranges of times in the past """
+    t1=-1000000
+    answer=[]
+    for name,t2 in ranges:
+        answer.append(sum((gaps>=t1)&(gaps<t2)))
+        t1=t2
+    return answer
+
+if __name__=='__main__':
+
+    df=get_table(set_id)
+    A=df.groupby(['problem_id','part_id','user_id'])
+    
+    Summary={}
+    for name,group in A:
+        problem,part,ID=name
+        success=(sum(group['score']) >0)
+        key=(problem,part,success);
+        if not key in Summary.keys():
+            Summary[key]={'time':0,'tries':0,'students':0}
+        Summary[key]['students']+=1    
+        T,L=total_time_trying(list(group['timestamp']))
+        Summary[key]['tries']+=L
+        Summary[key]['time']+=T.seconds
+        
+    Comb_Summary={}
+    #Combine successful and unsuccsessful for each problem and part
+    for key in Summary.keys():
+        if not key[:2] in Comb_Summary.keys():
+            Comb_Summary[key[:2]]={}
+        Comb_Summary[key[:2]][key[2]]=Summary[key]
+
+    print 'Summary for %s, at time=%s '%(set_id,str(datetime.datetime.now()))
+    print 'problem\t part \t\t FAILURE \t\t SUCCESS'
+    print '\t\t  stud\t time\t tries\t  stud\t time\t tries'
+    for key in sorted(Comb_Summary.keys()):
+        row=Comb_Summary[key]
+        print "%2d\t%2d"%key,
+        for x in [False,True]:
+            if not x in row.keys():
+                row[x]={'students': 0.01, 'time': 0, 'tries': 0}
+            row[x]['aver_time']=float(row[x]['time'])/row[x]['students']
+            row[x]['aver_tries']=float(row[x]['tries'])/row[x]['students']                                                    
+                                                        
+            print '\t%5.0f\t%5.2f\t%5.2f'%\
+                (row[x]['students'],row[x]['aver_time'],row[x]['aver_tries']),
+        print
+
---
+++ src/databases/describe_tables.py
@@ -0,0 +1,99 @@
+#The functions  md_table and pad_to copied and modified from csvtomd, https://github.com/mplewis/csvtomd
+import sys,os
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__))+'/../servers'))
+import pandas as pd
+import numpy as np
+from rest_server.tornado_database import  Connection
+print 'Usage:\ndescribe_tables.py mysql_username mysql_password [class_name (default=CSE103_Fall2015)]'
+if len(sys.argv)==1:
+    course = u'CSE103_Fall2015'
+    from rest_server.webwork_config import mysql_username, mysql_password
+elif len(sys.argv)==3:
+    course = u'CSE103_Fall2015'
+    mysql_username, mysql_password=sys.argv[1],sys.argv[2]
+elif len(sys.argv)==4:
+    mysql_username, mysql_password=sys.argv[1],sys.argv[2]
+    course=sys.argv[3]
+else:
+    print 'Usage:\ndescribe_tables.py mysql_username mysql_password [class_name (default=CSE103_Fall2015)]'
+print 'writing descriptions of tables of course '+course +' in databaseDescription.md file...'     
+
+tables = []
+
+def pad_to(unpadded, target_len):
+    """
+    Pad a string to the target length in characters, or return the original
+    string if it's longer than the target length.
+    """
+    under = target_len - len(unpadded)
+    if under <= 0:
+        return unpadded
+    return unpadded + (' ' * under)
+
+def md_table(df,  padding=1, divider='|', header_div='-'):
+    """
+    Convert a 2D array of items into a Markdown table.
+    padding: the number of padding spaces on either side of each divider
+    divider: the vertical divider to place between columns
+    header_div: the horizontal divider to place between the header row and
+        body cells
+    """
+    df=df.astype('str')
+    df.insert(0,'', map(str,df.index.values))
+    table= np.vstack((['', 'Field','Type', 'Null','Key','Default','Extra'],df.values))
+    # Output data buffer
+    output = ''
+    # Pad short rows to the length of the longest row to fix issues with
+    # rendering "jagged" CSV files
+    longest_row_len = max([len(row) for row in table])
+    for row in table:
+        while len(row) < longest_row_len:
+            row.append('')
+    # Get max length of any cell for each column
+    col_sizes = [max(map(len, col)) for col in zip(*table)]
+    # Set up the horizontal header dividers
+    header_divs = [None] * len(col_sizes)
+    num_cols = len(col_sizes)
+    # Pad header divs to the column size
+    for cell_num in range(num_cols):
+        header_divs[cell_num] = header_div * (col_sizes[cell_num] +
+                                              padding * 2)
+    # Trim first and last padding chars, if they exist
+    if padding > 0:
+        header_div_row = divider.join(header_divs)[padding:-padding]
+    else:
+        header_div_row = divider.join(header_divs)
+    # Pad each cell to the column size
+    for row in table:
+        for cell_num, cell in enumerate(row):
+            row[cell_num] = pad_to(cell, col_sizes[cell_num])
+    # Split out the header from the body
+    header = table[0]
+    body = table[1:]
+    # Build the inter-column dividers using the padding settings above
+    multipad = ' ' * padding
+    divider = multipad + divider + multipad
+    output += divider.join(header) + '\n'
+    output += header_div_row + '\n'
+    for row in body:
+        output += divider.join(row) + '\n'
+    # Strip the last newline
+    if output.endswith('\n'):
+        output = output[:-1]
+    return output
+
+
+
+
+if __name__ == '__main__':
+    conn = Connection('localhost','webwork',user=mysql_username,password=mysql_password)
+    result=conn.query("show tables")
+    table_names=[r for r in map(lambda x:x.values()[0], result) if r[:len(course)]==course]
+    DB=[]
+    for t in table_names:
+        res= conn.query("describe {}".format(t))
+        DB.append((t, pd.DataFrame([j for j in res])[['Field','Type', 'Null','Key','Default','Extra']]))
+    with open('./databaseDescription.md','w') as fileout:
+        for i,(t,df) in enumerate(DB):
+            print >> fileout, "### {}. {}\n***Description***:\n\n***Schema***:\n```\n{}\n```\n--------------------------------------------------".format(i+1,t,md_table(df)) 
+
