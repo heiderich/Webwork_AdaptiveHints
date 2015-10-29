efea4d21bd1c0bea6776d044e24b6b1d22030007
joemanley201 <joemanley201@gmail.com>
2015-10-29 10:03:35 -0700

    Local changes to make it work

--- src/servers/rest_server/webwork_config.py
+++ src/servers/rest_server/webwork_config.py
@@ -1,6 +1,6 @@
 """MySQL credential"""
-mysql_username = ''
-mysql_password = ''
+mysql_username = 'webworkWrite'
+mysql_password = 'webwork'
 
 """Local Webwork configurations"""
 webwork_dir = '/opt/webwork'
